e2b8bc9ca42067a085fe804d9d905f39e0efa2d9
Dipanshu-verma <vermadipanshu444@gmail.com>
2024-02-16 21:37:36 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Footer from './Components/footer/Footer';
+import Profile from './Components/profile/Profile';
 
 function App() {
   return (
-    <div className="relative overflow-hidden  h-[100vh] m-auto max-w-[330px] min-w-[250px] border border-red-500">
+    <div className="relative overflow-hidden h-[100vh] m-auto max-w-[330px] min-w-[250px]">
+    <Profile/>
    <Footer/>
 
        
---
+++ src/Components/profile/Membership.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Membership = () => {
+  return (
+    <div>
+      No active Membership
+    </div>
+  )
+}
+
+export default Membership
